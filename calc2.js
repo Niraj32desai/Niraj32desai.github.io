2332994db41503e443cf7d50892dd5d42481fcfa
Niraj32desai <123490917+Niraj32desai@users.noreply.github.com>
2023-05-27 16:45:57 +0530

    Update calc2.js after final sem3

--- calc2.js
+++ calc2.js
@@ -3,47 +3,59 @@ function aclick(input)
     v=input.value;
     op2=document.getElementsByClassName('op2')[0];
     op1=document.getElementsByClassName('op1')[0];
-    switch (v) {
+    switch (v) 
+    {
         case '=':
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML = eval(op1.innerHTML+op2.innerHTML);
             op1.innerHTML='';
             break;
+
         case '+':
-            //
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML = eval(op1.innerHTML+op2.innerHTML);
             op1.innerHTML='';
-            //
             op1.innerHTML = op2.innerHTML + v;
             op2.innerHTML='';
             break;
+
         case '-':
-            //
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML = eval(op1.innerHTML+op2.innerHTML);
             op1.innerHTML='';
-            //
             op1.innerHTML += op2.innerHTML + v;
             op2.innerHTML='';
             break;
+
         case '*':
-            //
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML = eval(op1.innerHTML+op2.innerHTML);
             op1.innerHTML='';
-            //
-                op1.innerHTML += op2.innerHTML + v;
-                op2.innerHTML='';
-                break;
-        case '/':
-            //
-            op2.innerHTML = eval(op1.innerHTML+op2.innerHTML);
-            op1.innerHTML='';
-            //
             op1.innerHTML += op2.innerHTML + v;
             op2.innerHTML='';
             break;
+
+        case '/':
+            
+            if (op2.innerHTML=="") break
+            op2.innerHTML = eval(op1.innerHTML+op2.innerHTML);
+            op1.innerHTML='';
+            op1.innerHTML += op2.innerHTML + v;
+            op2.innerHTML='';
+            break;
+
         case '%':
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML=eval(parseFloat(op2.innerHTML)/100);
             break;
+
         case 'DEL':
+            
             v1=op1.innerHTML;
             v2=op2.innerHTML;
             if (v2=='')
@@ -55,24 +67,54 @@ function aclick(input)
                 op2.innerHTML=v2.slice(0,-1);
             }
             break;
+
         case 'CE':
-            op2.innerHTML="";
+            
+            op2.innerHTML="0";
             break;
+
         case 'C':
+            
             op1.innerHTML="";
-            op2.innerHTML="";
+            op2.innerHTML="0";
             break;
+
         case "1/x":
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML=1/parseFloat(op2.innerHTML);
             break;
+
         case "x^2":
+            
+            if (op2.innerHTML=="") break
             op2.innerHTML=Math.pow(parseFloat(op2.innerHTML),2);
             break;
-        case "&Sqrt;x":
-            op2.innerHTML=Math.pow(parseFloat(op2.innerHTML),2);
+
+        case "√x":
+            
+            if (op2.innerHTML=="") break
+            op2.innerHTML=Math.pow(parseFloat(op2.innerHTML),0.5);
             break;
+
+        case "+/-":
+            
+            if (op2.innerHTML=="") break
+            op2.innerHTML*=-1;
+            break;
+            
         default:
-            op2.innerHTML += v;
+            
+            if (op2.innerHTML.toString().includes('.') && v.toString()=='.') break
+            else if (op2.innerHTML.toString()=='0' && v.toString()!='.')
+            {
+                op2.innerHTML="";
+                op2.innerHTML += v;
+            }
+            else if (op2.innerHTML.toString().length==0 && v.toString()=='0') break
+            else{
+                op2.innerHTML += v;
+            }
             break;
     }
 }
